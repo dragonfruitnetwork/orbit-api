38198e3e1f88ce30b8840d4a3822816ffa3311fe
Albie Spriddell <albion@dragonfruit.network>
2020-05-23 10:36:53 +0100

    namespace alignment

--- DragonFruit.Orbit.API/Requests/OsuBeatmapsetInfoRequest.cs
+++ DragonFruit.Orbit.API/Requests/OsuBeatmapsetInfoRequest.cs
@@ -1,7 +1,7 @@
 ﻿// Orbit API Copyright 2020 DragonFruit Network
 // Licensed under the MIT License - see the LICENSE file at the root of the project for more info
 
-namespace DragonFruit.Orbit.API.Requests.Beatmaps
+namespace DragonFruit.Orbit.API.Requests
 {
     public class OsuBeatmapsetInfoRequest : OrbitApiRequest
     {
