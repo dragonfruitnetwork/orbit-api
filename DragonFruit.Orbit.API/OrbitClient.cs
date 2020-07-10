2fc9cabf8d895a8df88244d8d28687b0d0bd5682
Albie <albion@dragonfruit.network>
2020-07-10 16:00:13 +0100

    change perform override to only apply to OrbitApiRequests

--- DragonFruit.Orbit.API/OrbitClient.cs
+++ DragonFruit.Orbit.API/OrbitClient.cs
@@ -74,7 +74,7 @@ namespace DragonFruit.Orbit.API
 
         #endregion
 
-        public override T Perform<T>(ApiRequest requestData) where T : class
+        public T Perform<T>(OrbitApiRequest requestData) where T : class
         {
             if (_token?.Expired ?? true)
             {
