fb6fbf91585bafb9f7480ed194ab60072b9ba15c
Albie <albion@dragonfruit.network>
2020-12-24 17:51:30 +0000

    add token invalidation function

---
+++ DragonFruit.Orbit.API/Auth/Extensions/OsuAuthExtensions.cs
@@ -0,0 +1,20 @@
+﻿// Orbit API Copyright 2020 DragonFruit Network
+// Licensed under the MIT License - see the LICENSE file at the root of the project for more info
+
+using DragonFruit.Orbit.Api.Auth.Requests;
+
+namespace DragonFruit.Orbit.Api.Auth.Extensions
+{
+    public static class OsuAuthExtensions
+    {
+        /// <summary>
+        /// Revokes the current session and signals the client the token is invalid.
+        /// This will throw if the request failed
+        /// </summary>
+        public static void RevokeCurrentSession<T>(this T client) where T : OrbitClient
+        {
+            client.Perform(new OsuSessionInvalidationRequest()).EnsureSuccessStatusCode();
+            client.InvalidateToken();
+        }
+    }
+}
