94e051a9cb754ad5e3dc214780e49298d1c3b2c2
Albie <albion@dragonfruit.network>
2020-12-31 16:44:48 +0000

    add beatmap info

---
+++ DragonFruit.Orbit.API/Beatmaps/Enums/BeatmapLeaderboardType.cs
@@ -0,0 +1,17 @@
+﻿// Orbit API Copyright 2020 DragonFruit Network
+// Licensed under the MIT License - see the LICENSE file at the root of the project for more info
+
+using DragonFruit.Orbit.Api.Utils;
+
+namespace DragonFruit.Orbit.Api.Beatmaps.Enums
+{
+    public enum BeatmapLeaderboardType
+    {
+        Global,
+        Country,
+
+        [ExternalValue("friend")]
+        Friends,
+        // GlobalCurrentMods, todo get ext value
+    }
+}
---
+++ DragonFruit.Orbit.API/Beatmaps/Requests/OsuBeatmapScoresRequest.cs
@@ -0,0 +1,24 @@
+﻿// Orbit API Copyright 2020 DragonFruit Network
+// Licensed under the MIT License - see the LICENSE file at the root of the project for more info
+
+using DragonFruit.Common.Data.Parameters;
+using DragonFruit.Orbit.Api.Ranking.Enums;
+using DragonFruit.Orbit.Api.Utils;
+
+namespace DragonFruit.Orbit.Api.Beatmaps.Requests
+{
+    public class OsuBeatmapScoresRequest : OsuBeatmapInfoRequest
+    {
+        protected override string Stub => $"{base.Stub}/scores";
+
+        public OsuBeatmapScoresRequest(uint id)
+            : base(id)
+        {
+        }
+
+        public LeaderboardType? LeaderboardType { get; set; }
+
+        [QueryParameter("type")]
+        protected string LeaderboardTypeName => LeaderboardType.ToExternalValue();
+    }
+}
