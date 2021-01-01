94c464e6cf55cf5e20efcc1d98c158c5d4fc1076
Albie <albion@dragonfruit.network>
2021-01-01 19:14:14 +0000

    update deploy script and nuget info

--- res/Nuget.ps1
+++ res/Nuget.ps1
@@ -1,21 +1,23 @@
 param ([string] $ApiKey, [string]$Suffix = "")
 
-#versioning info
+Set-Location -Path "..\"
+
+# versioning info
 $VERSION = "$(Get-Date -UFormat "%Y.%m%d").$($env:GITHUB_RUN_NUMBER)$($Suffix)"
 $WORKINGDIR = Get-Location
 
-#Build files
+# build files
 
 Write-Output "Building Orbit Version $VERSION"
 dotnet restore
 dotnet build -c Release /p:PackageVersion=$VERSION
 
-#pack into nuget files with the suffix if we have one
+# pack into nuget files with the suffix if we have one
 
 Write-Output "Publishing Orbit Version $VERSION"
-dotnet pack ".\DragonFruit.Orbit.API\DragonFruit.Orbit.API.csproj" -o $WORKINGDIR -c Release -p:PackageVersion=$VERSION
+dotnet pack ".\src\DragonFruit.Orbit.Api.csproj" -o $WORKINGDIR -c Release -p:PackageVersion=$VERSION
 
-#recursively push all nuget files created
+# recursively push all nuget files created
 
 Get-ChildItem -Path $WORKINGDIR -Filter *.nupkg -Recurse -File -Name | ForEach-Object {
     dotnet nuget push $_ --api-key $ApiKey --source https://api.nuget.org/v3/index.json --force-english-output
