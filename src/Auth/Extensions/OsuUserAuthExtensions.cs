4a27f8276fb3ddc6cb475e53cd718f75d19de034
Albie <albion@dragonfruit.network>
2021-12-07 19:53:06 +0000

    add async auth (general) extensions

--- src/Auth/Extensions/OsuUserAuthExtensions.cs
+++ src/Auth/Extensions/OsuUserAuthExtensions.cs
@@ -1,7 +1,7 @@
 ﻿// Orbit API Copyright (C) 2019-2021 DragonFruit Network
 // Licensed under the MIT License - see the LICENSE file at the root of the project for more info
 
-using System;
+using System.Threading.Tasks;
 using DragonFruit.Orbit.Api.Auth.Requests;
 
 namespace DragonFruit.Orbit.Api.Auth.Extensions
@@ -16,8 +16,18 @@ namespace DragonFruit.Orbit.Api.Auth.Extensions
         /// <param name="redirectUri">The url the client was redirected to</param>
         public static OsuAuthToken GetSessionToken<T>(this T client, string code, string redirectUri) where T : OrbitClient
         {
-            var request = new OsuUserAuthRequest(code, redirectUri);
-            return client.Perform<OsuAuthToken>(request);
+            return client.Perform<OsuAuthToken>(new OsuUserAuthRequest(code, redirectUri));
+        }
+
+        /// <summary>
+        /// Exchanges a user authorization code for a session-refresh token pair
+        /// </summary>
+        /// <param name="client">The <see cref="OrbitClient"/> to use</param>
+        /// <param name="code">The code from the user's redirect</param>
+        /// <param name="redirectUri">The url the client was redirected to</param>
+        public static Task<OsuAuthToken> GetSessionTokenAsync<T>(this T client, string code, string redirectUri) where T : OrbitClient
+        {
+            return client.PerformAsync<OsuAuthToken>(new OsuUserAuthRequest(code, redirectUri));
         }
 
         /// <summary>
@@ -27,13 +37,17 @@ namespace DragonFruit.Orbit.Api.Auth.Extensions
         /// <param name="refreshCode">The code to exchange for the new pair</param>
         public static OsuAuthToken RefreshSession<T>(this T client, string refreshCode) where T : OrbitClient
         {
-            if (string.IsNullOrEmpty(refreshCode))
-            {
-                throw new ArgumentNullException(nameof(refreshCode));
-            }
+            return client.Perform<OsuAuthToken>(new OsuUserRefreshRequest(refreshCode));
+        }
 
-            var request = new OsuUserRefreshRequest(refreshCode);
-            return client.Perform<OsuAuthToken>(request);
+        /// <summary>
+        /// Exchanges a refresh code for a new session-refresh token pair
+        /// </summary>
+        /// <param name="client">The <see cref="OrbitClient"/> to use</param>
+        /// <param name="refreshCode">The code to exchange for the new pair</param>
+        public static Task<OsuAuthToken> RefreshSessionAsync<T>(this T client, string refreshCode) where T : OrbitClient
+        {
+            return client.PerformAsync<OsuAuthToken>(new OsuUserRefreshRequest(refreshCode));
         }
 
         /// <summary>
@@ -44,6 +58,16 @@ namespace DragonFruit.Orbit.Api.Auth.Extensions
         public static OsuAuthToken RefreshSession<T>(this T client, OsuAuthToken currentToken) where T : OrbitClient
         {
             return RefreshSession(client, currentToken.RefreshToken);
+        }
+
+        /// <summary>
+        /// Exchanges a refresh code for a new session-refresh token pair
+        /// </summary>
+        /// <param name="client">The <see cref="OrbitClient"/> to use</param>
+        /// <param name="currentToken">The current token being used</param>
+        public static Task<OsuAuthToken> RefreshSessionAsync<T>(this T client, OsuAuthToken currentToken) where T : OrbitClient
+        {
+            return RefreshSessionAsync(client, currentToken.RefreshToken);
         }
     }
 }
