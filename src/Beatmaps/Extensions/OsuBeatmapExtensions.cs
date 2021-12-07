ff3d2ee98ec5b51a80dbd594b968a91ec6f760f7
Albie <albion@dragonfruit.network>
2021-12-07 19:52:39 +0000

    add async beatmap extensions

--- src/Beatmaps/Extensions/OsuBeatmapExtensions.cs
+++ src/Beatmaps/Extensions/OsuBeatmapExtensions.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License - see the LICENSE file at the root of the project for more info
 
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using DragonFruit.Orbit.Api.Beatmaps.Entities;
 using DragonFruit.Orbit.Api.Beatmaps.Enums;
 using DragonFruit.Orbit.Api.Beatmaps.Requests;
@@ -31,6 +32,26 @@ namespace DragonFruit.Orbit.Api.Beatmaps.Extensions
         }
 
         /// <summary>
+        /// Get the top 50 scores for a beatmap
+        /// </summary>
+        /// <param name="client">The <see cref="OrbitClient"/> to use</param>
+        /// <param name="mapId">The id of the map</param>
+        /// <param name="mode">The mode to return. Defaults to the map's original mode</param>
+        /// <param name="type">The <see cref="BeatmapLeaderboardType"/>. Global by default and will fail otherwise if the user is not a supporter</param>
+        /// <param name="mods">Array of mods to filter by. Probably need to be a supporter to send these</param>
+        public static Task<OsuBeamapScores> GetScoresAsync<T>(this T client, uint mapId, GameMode? mode = null, BeatmapLeaderboardType? type = null, IEnumerable<string> mods = null) where T : OrbitClient
+        {
+            var request = new OsuBeatmapScoresRequest(mapId)
+            {
+                Mode = mode,
+                Mods = mods,
+                Type = type
+            };
+
+            return client.PerformAsync<OsuBeamapScores>(request);
+        }
+
+        /// <summary>
         /// Get a beatmap's metadata by its online id
         /// </summary>
         /// <param name="client">The <see cref="OrbitClient"/> to use</param>
@@ -43,6 +64,21 @@ namespace DragonFruit.Orbit.Api.Beatmaps.Extensions
             };
 
             return client.Perform<OsuBeatmap>(request);
+        }
+
+        /// <summary>
+        /// Get a beatmap's metadata by its online id
+        /// </summary>
+        /// <param name="client">The <see cref="OrbitClient"/> to use</param>
+        /// <param name="mapId">The id of the map</param>
+        public static Task<OsuBeatmap> GetBeatmapAsync<T>(this T client, uint mapId) where T : OrbitClient
+        {
+            var request = new OsuBeatmapRequest
+            {
+                BeatmapId = mapId
+            };
+
+            return client.PerformAsync<OsuBeatmap>(request);
         }
 
         /// <summary>
@@ -61,6 +97,21 @@ namespace DragonFruit.Orbit.Api.Beatmaps.Extensions
         }
 
         /// <summary>
+        /// Get a beatmap's metadata by its MD5 hash
+        /// </summary>
+        /// <param name="client">The <see cref="OrbitClient"/> to use</param>
+        /// <param name="mapHash">The MD5 hash of the map</param>
+        public static Task<OsuBeatmap> GetBeatmapByHashAsync<T>(this T client, string mapHash) where T : OrbitClient
+        {
+            var request = new OsuBeatmapRequest
+            {
+                Hash = mapHash
+            };
+
+            return client.PerformAsync<OsuBeatmap>(request);
+        }
+
+        /// <summary>
         /// Get a beatmap's metadata by its original filename
         /// </summary>
         /// <param name="client">The <see cref="OrbitClient"/> to use</param>
@@ -73,6 +124,21 @@ namespace DragonFruit.Orbit.Api.Beatmaps.Extensions
             };
 
             return client.Perform<OsuBeatmap>(request);
+        }
+
+        /// <summary>
+        /// Get a beatmap's metadata by its original filename
+        /// </summary>
+        /// <param name="client">The <see cref="OrbitClient"/> to use</param>
+        /// <param name="fileName">The original, unedited name of the map</param>
+        public static Task<OsuBeatmap> GetBeatmapByFilenameAsync<T>(this T client, string fileName) where T : OrbitClient
+        {
+            var request = new OsuBeatmapRequest
+            {
+                Filename = fileName
+            };
+
+            return client.PerformAsync<OsuBeatmap>(request);
         }
     }
 }
