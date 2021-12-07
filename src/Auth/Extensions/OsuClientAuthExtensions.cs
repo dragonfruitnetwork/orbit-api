ec15c9dd043f5ff4bd7bb2298d1ae470056d8570
Albie <albion@dragonfruit.network>
2021-12-07 19:52:55 +0000

    add async auth (client) extensions

--- src/Auth/Extensions/OsuClientAuthExtensions.cs
+++ src/Auth/Extensions/OsuClientAuthExtensions.cs
@@ -1,6 +1,7 @@
 ﻿// Orbit API Copyright (C) 2019-2021 DragonFruit Network
 // Licensed under the MIT License - see the LICENSE file at the root of the project for more info
 
+using System.Threading.Tasks;
 using DragonFruit.Orbit.Api.Auth.Requests;
 
 namespace DragonFruit.Orbit.Api.Auth.Extensions
@@ -17,6 +18,18 @@ namespace DragonFruit.Orbit.Api.Auth.Extensions
         public static OsuAuthToken GetSessionToken<T>(this T client) where T : OrbitClient
         {
             return client.Perform<OsuAuthToken>(new OsuClientAuthRequest());
+        }
+
+        /// <summary>
+        /// Use the <see cref="OrbitClient"/> credentials to create a guest user access token
+        /// </summary>
+        /// <remarks>
+        /// Client credentials don't issue refresh tokens
+        /// </remarks>
+        /// <param name="client">The <see cref="OrbitClient"/> to use</param>
+        public static Task<OsuAuthToken> GetSessionTokenAsync<T>(this T client) where T : OrbitClient
+        {
+            return client.PerformAsync<OsuAuthToken>(new OsuClientAuthRequest());
         }
     }
 }
